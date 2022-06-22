6acfef047b2054235b55784884238bdb31e2dbe0
guillermouzner <guillermouz16@gmail.com>
2022-06-22 09:48:01 -0300

    swagger doc

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ import swaggerUI from "swagger-ui-express";
 
 const app = express();
 
-// const whiteList = ["*"];
+const whiteList = [process.env.ORIGIN];
 
-// app.use(
-//     cors({
-//         origin: function (origin, callback) {
-//             if (whiteList.includes(origin)) {
-//                 return callback(null, origin);
-//             }
-//             return callback(`Error de CORS: ${origin} No autorizado`);
-//         },
-//     })
-// );
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (whiteList.includes(origin)) {
+                return callback(null, origin);
+            }
+            return callback(`Error de CORS: ${origin} No autorizado`);
+        },
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +31,9 @@ app.use(cookieParser());
 app.use("/api/v1/doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/links", linkRouter);
+
+// solo para ejemplo
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
